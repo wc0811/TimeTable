6faf12e397336ec69d470d80dbedcb1ccdbd1995
86156 <wc0811@163.com>
2022-11-15 22:59:56 +0800

    Add SplashScreen

--- app/src/main/java/com/yueping/timetable/splash/SplashActivity.kt
+++ app/src/main/java/com/yueping/timetable/splash/SplashActivity.kt
@@ -2,40 +2,23 @@ package com.yueping.timetable.splash
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
-import com.dlasit.qamb.app.common.base.BaseActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.yueping.timetable.MainActivity
 import com.yueping.timetable.common.ext.navigateTo
 import com.yueping.timetable.databinding.ActivitySplashBinding
-import dagger.hilt.android.AndroidEntryPoint
 
 @SuppressLint("CustomSplashScreen")
-@AndroidEntryPoint
-class SplashActivity : BaseActivity<ActivitySplashBinding>() {
-
-    override val viewModel: SplashViewModel by viewModels()
-    override fun getViewBinding() = ActivitySplashBinding.inflate(layoutInflater)
-
-    override fun initData() {
-        super.initData()
-        viewModel.displaySplashView()
-    }
-
-    override fun observeFlow() {
-        super.observeFlow()
-        lifecycleScope.launchWhenResumed {
-            viewModel.navigateToNext.collect {
-                if (it) {
-                    navigateTo<MainActivity>(isFinish = true)
-                }
-            }
-        }
-    }
-
-    override fun beforeSetView(savedInstanceState: Bundle?) {
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
         savedInstanceState?.let {
             finish()
         }
+        val binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        splashScreen.setKeepOnScreenCondition { true }
+        navigateTo<MainActivity>(isFinish = true)
     }
 }
